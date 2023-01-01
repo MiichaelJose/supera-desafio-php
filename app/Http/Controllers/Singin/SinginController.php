777beb8904a39187b209fc1e1291b86536a58440
MiichaelJose <michaeljosecraft1@gmail.com>
2022-12-31 23:07:46 -0300

    alteração SinginController

--- app/Http/Controllers/Singin/SinginController.php
+++ app/Http/Controllers/Singin/SinginController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 
 use Illuminate\Routing\Redirector;
 use App\Http\Controllers\Home\HomeController;
+use Illuminate\Support\Facades\Hash;
 
 class SinginController extends Controller
 {
@@ -19,11 +20,19 @@ class SinginController extends Controller
 
     public function singin(Request $request) 
     {
-        $user = User::where('cpf', $request->cpf)
-            ->where('password', $request->password)
-            ->first();
+        // $user = User::where('cpf', $request->cpf)
+        //     ->where('password', bcrypt($request->password))
+        //     ->first();
         
-        if(!$user) {
+        // if(!$user) {
+        //     return redirect('/login')->with('status', 'Dados incorretos!');
+        // }
+
+
+        $user = User::where('cpf', $request->cpf)
+                    ->first();
+                    //dd(Hash::check($request->password, $user->password));
+        if (!Hash::check($request->password, $user->password)) {
             return redirect('/login')->with('status', 'Dados incorretos!');
         }
     
