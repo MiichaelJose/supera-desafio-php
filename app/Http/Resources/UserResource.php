fa4ca839fc3c650465ea525e59fc58be03232c6d
MiichaelJose <michaeljosecraft1@gmail.com>
2023-01-02 01:11:45 -0300

    alteração UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -11,6 +11,7 @@ class UserResource extends JsonResource
         return [
             'id'=> $this->id,
             'name' => $this->name,
+            'cpf' => $this->cpf,
             'role' => $this->role
         ];
     }
